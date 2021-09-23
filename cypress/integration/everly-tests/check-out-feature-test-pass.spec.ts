500a9b139432cda471a8426155829c7acd41504a
Jonathan <hoflercsc@gmail.com>
2021-09-23 11:27:24 -0400

    added check out feature tests
    added check out feature tests

---
+++ cypress/integration/everly-tests/check-out-feature-test-pass.spec.ts
@@ -0,0 +1,52 @@
+
+import LoginPage from '../everly-page-objects/LoginPage'
+import {Constants} from '../everly-page-objects/Constants'
+import HomePage from '../everly-page-objects/HomePage';
+import TestosteroneTestPage from '../everly-page-objects/TestosteroneTestPage';
+//import CheckoutPage from '../everly-page-objects/CheckoutPage'; // not actually created 
+
+describe('check out feature test pass', () => {
+
+    before(()=> {
+        cy.visit('/');
+    })
+
+    afterEach(() => {
+        Cypress.Cookies.preserveOnce('_sp_id.2ff8','tatari-session-cookie','_vwo_uuid_v2'); 
+    })
+    
+    it('login to everly', () => { 
+        LoginPage.userLogin(Constants.USER,Constants.PASSWORD)   
+    })
+
+    it('naviage to Testosterone test', () => {
+        HomePage.navigateToTestosteroneTest()
+    })
+
+    it('add to cart testosterone test', () => {
+        TestosteroneTestPage.addItemToCart()
+    })
+
+    it('add promotion code in cart', () => {
+        TestosteroneTestPage.addPromotionCode()
+    })
+
+    it('verify promo code discount and checkout', () => {
+        // Remeber no automatic discount applied just promo code
+        TestosteroneTestPage.verifyPromoSavingsNoDiscount()
+        TestosteroneTestPage.clickCheckOut()
+    })
+
+    it('fill check out shipping paymnet information form', () => {
+        //--- classes not created but this is how i would test check out
+        //--- this would pass because of correct informatiion entered
+        //CheckoutPage.correctShippingInformation()
+        //CheckoutPage.correctPaymentInformation()
+        //ChekoutPage.placeOrder.click()
+    })
+
+    after(() => {
+        cy.clearCookies()
+    })
+
+})
